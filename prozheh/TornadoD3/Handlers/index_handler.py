d6d7f899e4c5219db74643fdafe4d819d71d7d94
Mohammad Ali Nasrollahi <manrdm71@yahoo.com>
2015-01-28 14:53:15 +0330

    date index

--- prozheh/TornadoD3/Handlers/index_handler.py
+++ prozheh/TornadoD3/Handlers/index_handler.py
@@ -3,12 +3,36 @@ import tornado
 __author__ = 'mojtaba.banaie'
 # from pycket.session import SessionManager
 from models import *
+from datetime import datetime
+import khayyam3
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         news = News.select().order_by(News.id.desc())
         cnt=0
         list1=[]
+        kh=khayyam3.JalaliDate.today()
+        week=datetime.weekday(datetime.today())
+        if week==0:
+            week="دوشنبه"
+        elif week==1:
+            week="سه شنبه"
+        elif week==2:
+            week="چهار شنبه"
+        elif week==3:
+            week="پنج شنبه"
+        elif week==4:
+            week="جمعه"
+        elif week==5:
+            week="شنبه"
+        else:
+            week="یکشنبه"
+        # print(week)
+        kh=str(kh)
+        list2=[]
+        for k in kh.split("-"):
+            list2.append(k)
+        kh='%s/%s/%s'%(list2[0],list2[1],list2[2])+ "   " + week
         for n in news:
             n1=Author.select().where(Author.id==n.author).get().fn
             n2=Author.select().where(Author.id==n.author).get().ln
@@ -18,4 +42,4 @@ class IndexHandler(tornado.web.RequestHandler):
             cnt+=1
             if cnt==10:
                 break
-        self.render('index.html',UN= "Hello!",list1=list1)
+        self.render('index.html',UN= "Hello!",list1=list1,kh=kh)
